b59b8b98a73cec32d70d326cf1173a6e5c56e8f8
E Rodrigo Lopes <159098656+eRodrigoL@users.noreply.github.com>
2025-10-02 02:09:48 +0000

    fix `/migrations` endpoint connection bug

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,15 +1,17 @@
 import database from "infra/database.js";
 
 beforeAll(cleanDatabase);
+
 async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
-test("GET to /api/v1/migrations shoult return 200", async () => {
+test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
 
-  const responseBady = await response.json();
+  const responseBody = await response.json();
 
-  expect(Array.isArray(responseBady)).toBe(true);
-  expect(responseBady.length).toBeGreaterThan(0);
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
