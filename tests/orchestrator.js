6063d83c123b904e0b88d1c7f0f4323a6666909d
E Rodrigo Lopes <159098656+eRodrigoL@users.noreply.github.com>
2025-11-09 03:05:46 +0000

    fix `eslint` linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,15 +6,21 @@ async function waitForAllServices() {
   async function waitForAllServices() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     });
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
 }
 
-export default {
+const orchestrator = {
   waitForAllServices,
 };
+
+export default orchestrator;
